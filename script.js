a5aa6ffd3876651b040441327d5e7886a00aac86
wildanfajar <wildanfajarpamungkas964@gmail.com>
2026-05-01 01:50:01 +0700

    menambah countdown time

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ function stopAudio(id) {
   const audio = document.getElementById(id);
   audio.pause();
 }
+// logika countdown timer
+const countdownDate = new Date("June 15, 2026 00:00:00").getTime();
+
+const x = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "FESTIVAL STARTED!";
+  }
+}, 1000);
